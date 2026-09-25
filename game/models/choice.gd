329eddeb1cbe6fbeaccd05fb82876d1c48f24e01
Lucas TREIBER-LUSTIG <lucas.treiber-lustig@etu.unistra.fr>
2026-09-25 15:32:07 +0200

    add event models

--- game/models/choice.gd
+++ game/models/choice.gd
@@ -1,6 +1,7 @@
 extends Resource
 class_name Choice
 
-@export _name : String
-@export _description : String
-@export _sprite : 
+@export var _description : String
+@export var _money_impact : int
+@export var _required_item : Resource
+@export var _stats_modifiers : Array[Resource]
---
+++ game/models/event.gd
@@ -0,0 +1,7 @@
+extends Resource
+class_name Event
+
+@export var _title : String
+@export var _description : String
+@export var _image : Texture2D
+@export var _choices : Array[Choice]
